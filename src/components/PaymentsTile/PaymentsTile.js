600bad014e970bf9589154bc69e2245d046b6480
Devendra <devendranathreddy54@gmail.com>
2023-10-25 20:39:15 +0530

    added payment & settings tabs

---
+++ src/components/PaymentsTile/PaymentsTile.js
@@ -0,0 +1,14 @@
+import Navbar from "../Navbar/Navbar"
+import TilesBar from "../Navbar/TilesBar"
+import Paymnets from "./Paymnets"
+
+const PaymentsTile = () => {
+    return (
+        <div>
+            <Navbar />
+            <TilesBar />
+            <Paymnets />
+        </div>
+    )
+}
+export default PaymentsTile
---
+++ src/components/SettingsTile/Settings.js
@@ -0,0 +1,12 @@
+import Navbar from "../Navbar/Navbar"
+import TilesBar from "../Navbar/TilesBar"
+
+const Settings = () => {
+    return (
+        <div>
+            <Navbar />
+            <TilesBar />
+        </div>
+    )
+}
+export default Settings
