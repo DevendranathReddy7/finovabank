234eaa7a537bc77758d969360eb27eecd3c99eed
Devendra <devendranathreddy54@gmail.com>
2023-11-01 11:17:41 +0530

    respansive design for login screen

--- src/components/Login/StyledLogin.js
+++ src/components/Login/StyledLogin.js
@@ -4,6 +4,13 @@ export const StyledInput = styled.input`
 margin: ${(props) => (props.crn ? '13rem 30rem 0.5rem 20rem;' : '2rem 30rem 20px 20rem;')};
 height: 2rem;
 width: 18rem;
+@media (max-width: 480px) {
+    margin: ${(props) => (props.crn ? '13rem 30rem 0.5rem 2rem;' : '2rem 30rem 20px 2rem;')};
+
+}
+@media (max-width: 780px) {
+    margin: ${(props) => (props.crn ? '13rem 30rem 0.5rem 5rem;' : '2rem 30rem 20px 5rem;')};
+}
 `
 export const StyledButton = styled.button`
 margin:  1.3rem 30rem 20px 20rem;
@@ -13,10 +20,13 @@ background-color:rgb(40, 70, 158);
 border-style:none;
 color:white;
 font-weight:bold;
+@media (max-width: 480px) {
+    margin:  1.3rem 30rem 20px 2rem;
+}
+@media (max-width: 780px) {
+    margin:  1.3rem 30rem 20px 5rem;}
 `
 export const StyledDivLogin = styled.div`
-/* margin:10rem;
-padding-top: 5rem; */
-margin-left:10rem;
-height: 78vh;
+    margin-left:1rem;
+    height: 78vh;
 `
